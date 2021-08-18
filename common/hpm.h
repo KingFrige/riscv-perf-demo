26005d3aace38551a652e4f5040af5d1f6ec64dc
guangda.dong <guangda.dong@streamcomputing.com>
2021-08-18 20:47:03 +0800

    add hpmcounter read

---
+++ common/hpm.h
@@ -0,0 +1,41 @@
+#ifndef _HPM_H
+#define _HPM_H
+
+/*! @brief Macros for valid Event IDs */
+#define HPM_EVENTID_8 (1UL << 8)
+#define HPM_EVENTID_9 (1UL << 9)
+#define HPM_EVENTID_10 (1UL << 10)
+#define HPM_EVENTID_11 (1UL << 11)
+#define HPM_EVENTID_12 (1UL << 12)
+#define HPM_EVENTID_13 (1UL << 13)
+#define HPM_EVENTID_14 (1UL << 14)
+#define HPM_EVENTID_15 (1UL << 15)
+#define HPM_EVENTID_16 (1UL << 16)
+#define HPM_EVENTID_17 (1UL << 17)
+#define HPM_EVENTID_18 (1UL << 18)
+#define HPM_EVENTID_19 (1UL << 19)
+#define HPM_EVENTID_20 (1UL << 20)
+#define HPM_EVENTID_21 (1UL << 21)
+#define HPM_EVENTID_22 (1UL << 22)
+#define HPM_EVENTID_23 (1UL << 23)
+#define HPM_EVENTID_24 (1UL << 24)
+#define HPM_EVENTID_25 (1UL << 25)
+#define HPM_EVENTID_26 (1UL << 26)
+#define HPM_EVENTID_27 (1UL << 27)
+#define HPM_EVENTID_28 (1UL << 28)
+#define HPM_EVENTID_29 (1UL << 29)
+#define HPM_EVENTID_30 (1UL << 30)
+#define HPM_EVENTID_31 (1UL << 31)
+
+/*! @brief Macros for valid Event Class */
+#define HPM_EVENTCLASS_0 (0UL)
+#define HPM_EVENTCLASS_1 (1UL)
+#define HPM_EVENTCLASS_2 (2UL)
+#define HPM_EVENTCLASS_3 (3UL)
+#define HPM_EVENTCLASS_4 (4UL)
+#define HPM_EVENTCLASS_5 (5UL)
+#define HPM_EVENTCLASS_6 (6UL)
+#define HPM_EVENTCLASS_7 (7UL)
+#define HPM_EVENTCLASS_8 (8UL)
+
+#endif
--- perfMonCnt.c
+++ perfMonCnt.c
@@ -2,19 +2,73 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "util.h"
+#include "hpm.h"
 
 int main()
 {
 
   size_t instret, cycles;
+  size_t slotsIssed, fetchBubbles, recoveryCycles;
+  size_t brMispredRetired, machineClears, fetchLatencyCycles, opsExecutedCycles;
+  size_t memStallsAnyLoad, memStallsStores;
+
   instret = -read_csr(minstret);
   cycles  = -read_csr(mcycle);
+  printf("C0: %d instructions\n", (int)(instret));
+  printf("C0: %d cycles\n", (int)(cycles));
 
+  write_csr(mhpmcounter3, 0);
+  write_csr(mhpmevent3, HPM_EVENTID_8|HPM_EVENTCLASS_0);
+  write_csr(mhpmcounter4, 0);
+  write_csr(mhpmevent4, HPM_EVENTID_9|HPM_EVENTCLASS_0);
+
+  write_csr(mhpmcounter5, 0);
+  write_csr(mhpmevent5, HPM_EVENTID_8|HPM_EVENTCLASS_1);
+  write_csr(mhpmcounter6, 0);
+  write_csr(mhpmevent6, HPM_EVENTID_9|HPM_EVENTCLASS_1);
+  write_csr(mhpmcounter7, 0);
+  write_csr(mhpmevent7, HPM_EVENTID_10|HPM_EVENTCLASS_1);
+  write_csr(mhpmcounter8, 0);
+  write_csr(mhpmevent8, HPM_EVENTID_11|HPM_EVENTCLASS_1);
+  write_csr(mhpmcounter9, 0);
+  write_csr(mhpmevent9, HPM_EVENTID_12|HPM_EVENTCLASS_1);
+  write_csr(mhpmcounter10, 0);
+  write_csr(mhpmevent10, HPM_EVENTID_13|HPM_EVENTCLASS_1);
+  write_csr(mhpmcounter11, 0);
+  write_csr(mhpmevent11, HPM_EVENTID_14|HPM_EVENTCLASS_1);
+
+  printf("Hello Boom!\n");
+  printf("Hello Boom!\n");
+  printf("Hello Boom!\n");
+  printf("Hello Boom!\n");
+  printf("Hello Boom!\n");
   printf("Hello Boom!\n");
 
   instret += read_csr(minstret);
   cycles  += read_csr(mcycle);
 
-  printf("C%d: %d instructions\n", (int)(instret));
-  printf("C%d: %d cycles\n", (int)(cycles));
+  slotsIssed = read_csr(mhpmcounter3);
+  fetchBubbles = read_csr(mhpmcounter4);
+
+  recoveryCycles = read_csr(mhpmcounter5);
+  brMispredRetired = read_csr(mhpmcounter6);
+  machineClears = read_csr(mhpmcounter7);
+  fetchLatencyCycles = read_csr(mhpmcounter8);
+  opsExecutedCycles = read_csr(mhpmcounter9);
+  memStallsAnyLoad = read_csr(mhpmcounter10);
+  memStallsStores = read_csr(mhpmcounter11);
+
+  printf("C0: %d instructions\n", (int)(instret));
+  printf("C0: %d cycles\n", (int)(cycles));
+
+  printf("C0: %d slotsIssed\n", (int)(slotsIssed));
+  printf("C0: %d fetchBubbles\n", (int)(fetchBubbles));
+
+  printf("C0: %d recoveryCycles\n", (int)(recoveryCycles));
+  printf("C0: %d brMispredRetired\n", (int)(brMispredRetired));
+  printf("C0: %d machineClears\n", (int)(machineClears));
+  printf("C0: %d fetchLatencyCycles\n", (int)(fetchLatencyCycles));
+  printf("C0: %d opsExecutedCycles\n", (int)(opsExecutedCycles));
+  printf("C0: %d memStallsAnyLoad\n", (int)(memStallsAnyLoad));
+  printf("C0: %d memStallsStores\n", (int)(memStallsStores));
 }
