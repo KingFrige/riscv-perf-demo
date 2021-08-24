636e92d63862cfa761014ff2896bad97c1f76ae0
guangda.dong <guangda.dong@streamcomputing.com>
2021-08-24 17:25:42 +0800

    update demo

--- perfMonCnt.c
+++ perfMonCnt.c
@@ -1,8 +1,16 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "util.h"
 #include "hpm.h"
+
+#pragma GCC optimize ("unroll-loops")
+
+typedef float t;
+
+static const int RBM = 4, RBN = 5, RBK = 6;
+static const int CBM = 24, CBN = 25, CBK = 24;
 
 extern int topDownCntSet();
 extern int topDownCntGet();
@@ -11,11 +19,26 @@ int main()
 {
   topDownCntSet();
 
-  printf("Hello Boom!\n");
-  printf("Hello Boom!\n");
-  printf("Hello Boom!\n");
-  printf("Hello Boom!\n");
-  printf("Hello Boom!\n");
+  const int R = 8;
+  int m, n, p;
+  uint64_t s = 0xdeadbeefU;
+
+  m = CBM;
+  n = CBN;
+  p = CBK;
+
+  t a[m*p];
+  t b[p*n];
+  t c[m*n];
+
+  for (size_t i = 0; i < m; i++)
+    for (size_t j = 0; j < p; j++)
+      a[i*p+j] = (t)(s = lfsr(s));
+  for (size_t i = 0; i < p; i++)
+    for (size_t j = 0; j < n; j++)
+      b[i*n+j] = (t)(s = lfsr(s));
+  memset(c, 0, m*n*sizeof(c[0]));
+
   printf("Hello Boom!\n");
 
   topDownCntGet();
