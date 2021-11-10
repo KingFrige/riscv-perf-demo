786b2802863aa9e0e46c880e9a32f44ba493ebd3
korben.dong <ghostyuan@outlook.com>
2021-11-10 15:34:58 +0800

    fixed info register addr error

--- common/insn_info_hpm.c
+++ common/insn_info_hpm.c
@@ -16,28 +16,28 @@ int insnInfoCntSet(){
   write_csr(minstret, 0);
   write_csr(mcycle, 0);
 
-  SET_PERFCNT( 3,  8, 4); // int load
-  SET_PERFCNT( 4,  9, 4); // int store
-  SET_PERFCNT( 5, 10, 4); // int amo
-  SET_PERFCNT( 6, 11, 4); // int system
-  SET_PERFCNT( 7, 12, 4); // int arith
-  SET_PERFCNT( 8, 13, 4); // int branch
-  SET_PERFCNT( 9, 14, 4); // int jal
-  SET_PERFCNT(10, 15, 4); // int jalr
-  SET_PERFCNT(11, 16, 4); // int mul
-  SET_PERFCNT(12, 17, 4); // int div
-  SET_PERFCNT(13, 18, 4); // fp
-  SET_PERFCNT(14, 19, 4); // fp load
-  SET_PERFCNT(15, 20, 4); // fp store
-  SET_PERFCNT(16, 21, 4); // fp add
-  SET_PERFCNT(17, 22, 4); // fp mul
-  SET_PERFCNT(18, 23, 4); // fp mul-add
-  SET_PERFCNT(19, 24, 4); // fp div
-  SET_PERFCNT(20, 25, 4); // fp other
+  SET_PERFCNT( 3,  8, 5); // int load
+  SET_PERFCNT( 4,  9, 5); // int store
+  SET_PERFCNT( 5, 10, 5); // int amo
+  SET_PERFCNT( 6, 11, 5); // int system
+  SET_PERFCNT( 7, 12, 5); // int arith
+  SET_PERFCNT( 8, 13, 5); // int branch
+  SET_PERFCNT( 9, 14, 5); // int jal
+  SET_PERFCNT(10, 15, 5); // int jalr
+  SET_PERFCNT(11, 16, 5); // int mul
+  SET_PERFCNT(12, 17, 5); // int div
+  SET_PERFCNT(13, 18, 5); // fp
+  SET_PERFCNT(14, 19, 5); // fp load
+  SET_PERFCNT(15, 20, 5); // fp store
+  SET_PERFCNT(16, 21, 5); // fp add
+  SET_PERFCNT(17, 22, 5); // fp mul
+  SET_PERFCNT(18, 23, 5); // fp mul-add
+  SET_PERFCNT(19, 24, 5); // fp div
+  SET_PERFCNT(20, 25, 5); // fp other
 
-  SET_PERFCNT(21,  8, 5); // exception
-  SET_PERFCNT(22,  9, 5); // f1 clear
-  SET_PERFCNT(23, 10, 5); // f2 clear
+  SET_PERFCNT(21,  8, 6); // exception
+  SET_PERFCNT(22,  9, 6); // f1 clear
+  SET_PERFCNT(23, 10, 6); // f2 clear
 
   SET_PERFCNT(24, 10, 0); // br retired
   SET_PERFCNT(25, 11, 0); // br retired and taken
