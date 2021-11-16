7b2c658adad16955fe85ea21afda366b7e71f6ca
korben.dong <ghostyuan@outlook.com>
2021-11-16 10:34:00 +0800

    update linux perf cnt

--- common/hpm.h
+++ common/hpm.h
@@ -9,8 +9,8 @@
   printf(fmt, (int)(read_csr(mhpmcounter ## mhpmcnt)));
 
 /*! @brief Macros for valid Event IDs */
-#define HPM_EVENTID_8 (1UL << 8)
-#define HPM_EVENTID_9 (1UL << 9)
+#define HPM_EVENTID_8  (1UL << 8)
+#define HPM_EVENTID_9  (1UL << 9)
 #define HPM_EVENTID_10 (1UL << 10)
 #define HPM_EVENTID_11 (1UL << 11)
 #define HPM_EVENTID_12 (1UL << 12)
@@ -33,6 +33,15 @@
 #define HPM_EVENTID_29 (1UL << 29)
 #define HPM_EVENTID_30 (1UL << 30)
 #define HPM_EVENTID_31 (1UL << 31)
+#define HPM_EVENTID_32 (1UL << 32)
+#define HPM_EVENTID_33 (1UL << 33)
+#define HPM_EVENTID_34 (1UL << 34)
+#define HPM_EVENTID_35 (1UL << 35)
+#define HPM_EVENTID_36 (1UL << 36)
+#define HPM_EVENTID_37 (1UL << 37)
+#define HPM_EVENTID_38 (1UL << 38)
+#define HPM_EVENTID_39 (1UL << 39)
+#define HPM_EVENTID_40 (1UL << 40)
 
 /*! @brief Macros for valid Event Class */
 #define HPM_EVENTCLASS_0 (0UL)
--- common/linux_perf_hpm.c
+++ common/linux_perf_hpm.c
@@ -9,6 +9,49 @@
 #include "util.h"
 #include "hpm.h"
 
+int linuxPerfCntSet(){
+  write_csr(scounteren, -1);
+  write_csr(mcounteren, -1);
+
+  write_csr(minstret, 0);
+  write_csr(mcycle, 0);
+
+  SET_PERFCNT( 3,  8, 0);
+  SET_PERFCNT( 4,  9, 0);
+  SET_PERFCNT( 5, 10, 0);
+  SET_PERFCNT( 6, 11, 0);
+
+  SET_PERFCNT( 7, 12, 0);
+  SET_PERFCNT( 8, 13, 1);
+  SET_PERFCNT( 9, 14, 1);
+  SET_PERFCNT(10, 15, 1);
+  SET_PERFCNT(11, 16, 1);
+  SET_PERFCNT(12, 17, 8);
+
+  SET_PERFCNT(13, 19, 8);
+  SET_PERFCNT(14, 20, 8);
+  SET_PERFCNT(15, 21, 8);
+  SET_PERFCNT(16, 22, 8);
+
+  SET_PERFCNT(17, 23, 8);
+  SET_PERFCNT(18, 24, 8);
+  SET_PERFCNT(19, 25, 8);
+  SET_PERFCNT(20, 26, 8);
+  SET_PERFCNT(21, 27, 8);
+  SET_PERFCNT(22, 28, 8);
+
+  SET_PERFCNT(23, 29, 8);
+  SET_PERFCNT(24, 30, 8);
+  SET_PERFCNT(25, 31, 8);
+  SET_PERFCNT(26, 32, 8);
+  SET_PERFCNT(27, 33, 8);
+  SET_PERFCNT(28, 34, 8);
+  SET_PERFCNT(29, 35, 8);
+  SET_PERFCNT(30, 36, 8);
+
+  return 0;
+}
+
 int linuxPerfCntGet(){
   size_t instret, cycles;
 
@@ -18,41 +61,43 @@ int linuxPerfCntGet(){
   printf("instructions:%d\n", (int)(instret));
   printf("cpu_cycle:%d\n", (int)(cycles));
 
-  SHOW_PERFCNT("cache_references:%d\n", 3);
-  SHOW_PERFCNT("branches:%d\n", 4);
-  SHOW_PERFCNT("branch_misses:%d\n", 5);
-  SHOW_PERFCNT("bus_cycles:%d\n", 6);
+  SHOW_PERFCNT("l1_icache_loads:%d\n", 3);
+  SHOW_PERFCNT("l1_icache_load_misses:%d\n", 4);
+  SHOW_PERFCNT("l1_icache_prefetches:%d\n", 5);
+  SHOW_PERFCNT("l1_icache_prefetch_misses:%d\n", 6);
 
-  SHOW_PERFCNT("l1_icache_loads:%d\n", 7);
-  SHOW_PERFCNT("l1_icache_load_misses:%d\n", 8);
-  SHOW_PERFCNT("l1_icache_prefetches:%d\n", 9);
-  SHOW_PERFCNT("l1_icache_prefetche_misses:%d\n", 10);
+  SHOW_PERFCNT("l1_dcache_loads:%d\n", 7);
+  SHOW_PERFCNT("l1_dcache_load_misses:%d\n", 8);
+  SHOW_PERFCNT("l1_dcache_stores:%d\n", 9);
+  SHOW_PERFCNT("l1_dcache_store_misses:%d\n", 10);
+  SHOW_PERFCNT("l1_dcache_prefetches:%d\n", 11);
+  SHOW_PERFCNT("l1_dcache_prefetch_misses:%d\n", 12);
 
-  SHOW_PERFCNT("l1_dcache_loads:%d\n", 11);
-  SHOW_PERFCNT("l1_dcache_load_misses:%d\n", 12);
-  SHOW_PERFCNT("l1_dcache_stores:%d\n", 13);
-  SHOW_PERFCNT("l1_dcache_store_misses:%d\n", 14);
-  SHOW_PERFCNT("l1_dcache_prefetches:%d\n", 15);
-  SHOW_PERFCNT("l1_dcache_prefetche_misses:%d\n", 16);
+  SHOW_PERFCNT("itlb_loads:%d\n", 13);
+  SHOW_PERFCNT("itlb_load_misses:%d\n", 14);
+  SHOW_PERFCNT("itlb_prefetches:%d\n", 15);
+  SHOW_PERFCNT("itlb_prefetch_misses:%d\n", 16);
 
-  SHOW_PERFCNT("l2_cache_loads:%d\n", 17);
-  SHOW_PERFCNT("l2_cache_load_misses:%d\n", 18);
-  SHOW_PERFCNT("l2_cache_stores:%d\n", 19);
-  SHOW_PERFCNT("l2_cache_store_misses:%d\n", 20);
-  SHOW_PERFCNT("l2_cache_prefetches:%d\n", 21);
-  SHOW_PERFCNT("l2_cache_prefetche_misses:%d\n", 22);
+  SHOW_PERFCNT("dtlb_loads:%d\n", 17);
+  SHOW_PERFCNT("dtlb_load_misses:%d\n", 18);
+  SHOW_PERFCNT("dtlb_stores:%d\n", 19);
+  SHOW_PERFCNT("dtlb_store_misses:%d\n", 20);
+  SHOW_PERFCNT("dtlb_prefetches:%d\n", 21);
+  SHOW_PERFCNT("dtlb_prefetch_misses:%d\n", 22);
 
-  SHOW_PERFCNT("dtlb_loads:%d\n", 23);
-  SHOW_PERFCNT("dtlb_load_misses:%d\n", 24);
-  SHOW_PERFCNT("dtlb_stores:%d\n", 25);
-  SHOW_PERFCNT("dtlb_store_misses:%d\n", 26);
-  SHOW_PERFCNT("dtlb_prefetches:%d\n", 27);
-  SHOW_PERFCNT("dtlb_prefetch_misses:%d\n", 28);
+  SHOW_PERFCNT("cache_references:%d\n", 23); // llc references -> hit
+  SHOW_PERFCNT("cache_misses:%d\n", 24);     // llc misses
+  SHOW_PERFCNT("llc_cache_loads:%d\n", 25);
+  SHOW_PERFCNT("llc_cache_load_misses:%d\n", 26);
+  SHOW_PERFCNT("llc_cache_stores:%d\n", 27);
+  SHOW_PERFCNT("llc_cache_store_misses:%d\n", 28);
+  SHOW_PERFCNT("llc_cache_prefetches:%d\n", 29);
+  SHOW_PERFCNT("llc_cache_prefetch_misses:%d\n", 30);
 
-  SHOW_PERFCNT("itlb_loads:%d\n", 29);
-  SHOW_PERFCNT("itlb_load_misses:%d\n", 30);
-
-  SHOW_PERFCNT("branch_loads:%d\n", 31);
+  // SHOW_PERFCNT("bus_cycles:%d\n", 31);
+  // SHOW_PERFCNT("branches:%d\n", 29);
+  // SHOW_PERFCNT("branch_misses:%d\n", 30);
+  // SHOW_PERFCNT("branch_loads:%d\n", 31);
   // SHOW_PERFCNT("branch_load_misses:%d\n", 32);
 
   return 0;
